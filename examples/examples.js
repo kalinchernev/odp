07ede78c2ab4b954f057301157e58bacb652cc02
Kalin Chernev <kalin.chernev@gmail.com>
2016-10-02 08:43:24 +0000

    Linting based on recommended.

--- examples/examples.js
+++ examples/examples.js
@@ -1,19 +1,24 @@
-var odp = require('../lib/odp');
+'use strict'
+
+// console.log is allowed in this file.
+/* eslint no-console: 0 */
+
+var odp = require('../lib/odp')
 
 // Get list of all datasets.
 odp.getDatasets().then((data) => {
-  console.log(data);
-});
+  console.log(data)
+})
 
 // Get a range of the list of datasets:
 odp.getDatasets({query: {limit: 100, offset: 1}}).then((data) => {
-  console.log(data);
-});
+  console.log(data)
+})
 
 // Get all tags:
 odp.getTags().then((data) => {
-  console.log(data);
-});
+  console.log(data)
+})
 
 // Search through tags:
 odp.getTags({
@@ -22,15 +27,15 @@ odp.getTags({
     all_fields: true
   }
 }).then((data) => {
-  console.log(data);
-});
+  console.log(data)
+})
 
 // Get details about a dataset:
-odp.getDataset({ body: {id: 'dgt-translation-memory'}}).then((data) => {
-  console.log(data);
-});
+odp.getDataset({body: {id: 'dgt-translation-memory'}}).then((data) => {
+  console.log(data)
+})
 
 // Search for a dataset:
-odp.datasetSearch({ body: {q: "forest"}}).then((data) => {
-  console.log(data);
-});
+odp.datasetSearch({body: {q: 'forest'}}).then((data) => {
+  console.log(data)
+})
--- test/test.js
+++ test/test.js
@@ -1,26 +1,26 @@
-var should = require('chai').should();
-var odpClient = require("../lib/odp");
+var should = require('chai').should()
+var odpClient = require('../lib/odp')
 
 describe('ODP module', function () {
   describe('the client', function () {
     it('has a method getDatasets', function (done) {
-      odpClient.should.have.property('getDatasets');
-      done();
-    });
+      odpClient.should.have.property('getDatasets')
+      done()
+    })
 
     it('has a method getTags', function (done) {
-      odpClient.should.have.property('getTags');
-      done();
-    });
+      odpClient.should.have.property('getTags')
+      done()
+    })
 
     it('has a method getDataset', function (done) {
-      odpClient.should.have.property('getDataset');
-      done();
-    });
-    
+      odpClient.should.have.property('getDataset')
+      done()
+    })
+
     it('has a method datasetSearch', function (done) {
-      odpClient.should.have.property('datasetSearch');
-      done();
-    });    
-  });
-});
+      odpClient.should.have.property('datasetSearch')
+      done()
+    })
+  })
+})
