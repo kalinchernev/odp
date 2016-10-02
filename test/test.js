50719900662bb592bf730914f84e00334b768215
Kalin Chernev <kalin.chernev@gmail.com>
2016-10-02 07:15:55 +0000

    Generalizing the request helper.

--- test/test.js
+++ test/test.js
@@ -7,8 +7,20 @@ describe('ODP module', function () {
       odpClient.should.have.property('getDatasets');
       done();
     });
+
+    it('has a method getTags', function (done) {
+      odpClient.should.have.property('getTags');
+      done();
+    });
+
+    it('has a method getDataset', function (done) {
+      odpClient.should.have.property('getDataset');
+      done();
+    });
     
-    
+    it('has a method datasetSearch', function (done) {
+      odpClient.should.have.property('datasetSearch');
+      done();
+    });    
   });
 });
-
