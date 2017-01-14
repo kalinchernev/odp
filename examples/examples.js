e056f7b69e6d1967b90f8068561280f14d2ccb4a
Kalin Chernev <kalin.chernev@gmail.com>
2017-01-14 19:53:03 +0000

    Small updates

--- examples/examples.js
+++ examples/examples.js
@@ -6,36 +6,28 @@
 var odp = require('../lib/odp')
 
 // Get list of all datasets.
-odp.getDatasets().then((data) => {
-  console.log(data)
-})
+odp.getDatasets().then((data) => console.log(data))
 
 // Get a range of the list of datasets:
-odp.getDatasets({query: {limit: 100, offset: 1}}).then((data) => {
-  console.log(data)
-})
+odp.getDatasets({query: {limit: 100, offset: 1}})
+  .then((data) => console.log(data))
 
 // Get all tags:
-odp.getTags().then((data) => {
-  console.log(data)
-})
+odp.getTags().then(data => console.log(data))
 
 // Search through tags:
 odp.getTags({
   query: {
     vocabulary_id: 'some_id',
     all_fields: true
-  }
-}).then((data) => {
-  console.log(data)
-})
+    }
+  })
+  .then((data) => console.log(data))
 
 // Get details about a dataset:
-odp.getDataset({body: {id: 'dgt-translation-memory'}}).then((data) => {
-  console.log(data)
-})
+odp.getDataset({body: {id: 'dgt-translation-memory'}})
+  .then((data) => console.log(data))
 
 // Search for a dataset:
-odp.datasetSearch({body: {q: 'forest'}}).then((data) => {
-  console.log(data)
-})
+odp.datasetSearch({body: {q: 'forest'}})
+  .then((data) => console.log(data))
